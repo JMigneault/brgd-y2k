44be0739ca636f3bb4f394007772178a32158804
JMigneault <jacrovm@gmail.com>
2021-04-25 15:39:02 -0400

    Fix mouse cursor not resetting after exiting ETW.

--- Assets/overworld/Scripts/DestroyTSLeftover.cs
+++ Assets/overworld/Scripts/DestroyTSLeftover.cs
@@ -7,12 +7,16 @@ public class DestroyTSLeftover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // delete DontDeleteOnLoad objects from TS
         GameObject ch = GameObject.Find("Cursor Handler");
         GameObject tm = GameObject.Find("TitleMusic");
         GameObject dh = GameObject.Find("Difficulty Handler");
         GameObject.Destroy(ch);
         GameObject.Destroy(tm);
         GameObject.Destroy(dh);
+
+        // reset cursor from etw
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto); 
     }
 
     // Update is called once per frame
